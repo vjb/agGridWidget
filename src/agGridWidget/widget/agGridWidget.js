e0a2c446b5da2af19d52cc156659444209b66da4
vincent beltrani <vincent.beltrani@mendix.com>
2019-07-23 11:08:42 -0500

    column sort is now toggable

--- src/agGridWidget/widget/agGridWidget.js
+++ src/agGridWidget/widget/agGridWidget.js
@@ -36,58 +36,79 @@ define([
 
     "agGridWidget/lib/jquery-1.11.2",
     "dojo/text!agGridWidget/widget/template/agGridWidget.html"
-], function (declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml, dojoEvent, _jQuery, widgetTemplate) {
+], function(
+    declare,
+    _WidgetBase,
+    _TemplatedMixin,
+    dom,
+    dojoDom,
+    dojoProp,
+    dojoGeometry,
+    dojoClass,
+    dojoStyle,
+    dojoConstruct,
+    dojoArray,
+    lang,
+    dojoText,
+    dojoHtml,
+    dojoEvent,
+    _jQuery,
+    widgetTemplate
+) {
     "use strict";
 
     var $ = _jQuery.noConflict(true);
 
     // Declare widget's prototype.
-    return declare("agGridWidget.widget.agGridWidget", [ _WidgetBase, _TemplatedMixin ], {
-        // _TemplatedMixin will create our dom node using this HTML template.
-        templateString: widgetTemplate,
+    return declare(
+        "agGridWidget.widget.agGridWidget",
+        [_WidgetBase, _TemplatedMixin],
+        {
+            // _TemplatedMixin will create our dom node using this HTML template.
+            templateString: widgetTemplate,
 
-        // DOM elements
-        inputNodes: null,
-        colorSelectNode: null,
-        colorInputNode: null,
-        infoTextNode: null,
+            // DOM elements
+            inputNodes: null,
+            colorSelectNode: null,
+            colorInputNode: null,
+            infoTextNode: null,
 
-        // Parameters configured in the Modeler.
-        mfToExecute: "",
-        messageString: "",
-        backgroundColor: "",
+            // Parameters configured in the Modeler.
+            mfToExecute: "",
+            messageString: "",
+            backgroundColor: "",
 
-        // Internal variables. Non-primitives created in the prototype are shared between all widget instances.
-        _handles: null,
-        _contextObj: null,
-        _alertDiv: null,
-        _readOnly: false,
+            // Internal variables. Non-primitives created in the prototype are shared between all widget instances.
+            _handles: null,
+            _contextObj: null,
+            _alertDiv: null,
+            _readOnly: false,
 
-        // dojo.declare.constructor is called to construct the widget instance. Implement to initialize non-primitive properties.
-        constructor: function () {
-            logger.debug(this.id + ".constructor");
-            this._handles = [];
-        },
+            // dojo.declare.constructor is called to construct the widget instance. Implement to initialize non-primitive properties.
+            constructor: function() {
+                logger.debug(this.id + ".constructor");
+                this._handles = [];
+            },
 
-        // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
-        postCreate: function () {
-            logger.debug(this.id + ".postCreate");
+            // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
+            postCreate: function() {
+                logger.debug(this.id + ".postCreate");
 
-            if (this.readOnly || this.get("disabled") || this.readonly) {
-              this._readOnly = true;
-            }
+                if (this.readOnly || this.get("disabled") || this.readonly) {
+                    this._readOnly = true;
+                }
 
-            this._updateRendering();
-            this._setupEvents();
-        },
+                this._updateRendering();
+                this._setupEvents();
+            },
 
-        // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
-        update: function (obj, callback) {
-            logger.debug(this.id + ".update");
+            // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
+            update: function(obj, callback) {
+                logger.debug(this.id + ".update");
 
-            var gridDiv = document.querySelector('#myGrid');
+                var gridDiv = document.querySelector("#myGrid");
 
-            /*
+                /*
             var gridOptions = {
                 columnDefs: [
                     {headerName: 'Name', field: 'name'},
@@ -100,168 +121,183 @@ define([
                     {name: 'Kevin', role: 'Manager'}
                 ]
             };
-*/          var inputJSON = obj.get(this.gridDataJSON);
-            var gridOptions = JSON.parse(inputJSON);
-    
-            new agGrid.Grid(gridDiv, gridOptions);
-            this._contextObj = obj;
-            this._resetSubscriptions();
-            this._updateRendering(callback); // We're passing the callback to updateRendering to be called after DOM-manipulation
-        },
+*/ var inputJSON = obj.get(
+                    this.gridDataJSON
+                );
+                var gridOptions = JSON.parse(inputJSON);
+                if (this.allowSort) {
+                    gridOptions.defaultColDef = { sortable: true };
+                }
+                new agGrid.Grid(gridDiv, gridOptions);
+                this._contextObj = obj;
+                this._resetSubscriptions();
+                this._updateRendering(callback); // We're passing the callback to updateRendering to be called after DOM-manipulation
+            },
 
-        // mxui.widget._WidgetBase.enable is called when the widget should enable editing. Implement to enable editing if widget is input widget.
-        enable: function () {
-          logger.debug(this.id + ".enable");
-        },
+            // mxui.widget._WidgetBase.enable is called when the widget should enable editing. Implement to enable editing if widget is input widget.
+            enable: function() {
+                logger.debug(this.id + ".enable");
+            },
 
-        // mxui.widget._WidgetBase.enable is called when the widget should disable editing. Implement to disable editing if widget is input widget.
-        disable: function () {
-          logger.debug(this.id + ".disable");
-        },
+            // mxui.widget._WidgetBase.enable is called when the widget should disable editing. Implement to disable editing if widget is input widget.
+            disable: function() {
+                logger.debug(this.id + ".disable");
+            },
 
-        // mxui.widget._WidgetBase.resize is called when the page's layout is recalculated. Implement to do sizing calculations. Prefer using CSS instead.
-        resize: function (box) {
-          logger.debug(this.id + ".resize");
-        },
+            // mxui.widget._WidgetBase.resize is called when the page's layout is recalculated. Implement to do sizing calculations. Prefer using CSS instead.
+            resize: function(box) {
+                logger.debug(this.id + ".resize");
+            },
 
-        // mxui.widget._WidgetBase.uninitialize is called when the widget is destroyed. Implement to do special tear-down work.
-        uninitialize: function () {
-          logger.debug(this.id + ".uninitialize");
-            // Clean up listeners, helper objects, etc. There is no need to remove listeners added with this.connect / this.subscribe / this.own.
-        },
+            // mxui.widget._WidgetBase.uninitialize is called when the widget is destroyed. Implement to do special tear-down work.
+            uninitialize: function() {
+                logger.debug(this.id + ".uninitialize");
+                // Clean up listeners, helper objects, etc. There is no need to remove listeners added with this.connect / this.subscribe / this.own.
+            },
 
-        // We want to stop events on a mobile device
-        _stopBubblingEventOnMobile: function (e) {
-            logger.debug(this.id + "._stopBubblingEventOnMobile");
-            if (typeof document.ontouchstart !== "undefined") {
-                dojoEvent.stop(e);
-            }
-        },
+            // We want to stop events on a mobile device
+            _stopBubblingEventOnMobile: function(e) {
+                logger.debug(this.id + "._stopBubblingEventOnMobile");
+                if (typeof document.ontouchstart !== "undefined") {
+                    dojoEvent.stop(e);
+                }
+            },
 
-        // Attach events to HTML dom elements
-        _setupEvents: function () {
-            logger.debug(this.id + "._setupEvents");
-            
-           
-        },
+            // Attach events to HTML dom elements
+            _setupEvents: function() {
+                logger.debug(this.id + "._setupEvents");
+            },
 
-        _execMf: function (mf, guid, cb) {
-            logger.debug(this.id + "._execMf");
-            if (mf && guid) {
-                mx.ui.action(mf, {
-                    params: {
-                        applyto: "selection",
-                        guids: [guid]
-                    },
-                    callback: lang.hitch(this, function (objs) {
-                        if (cb && typeof cb === "function") {
-                            cb(objs);
-                        }
-                    }),
-                    error: function (error) {
-                        console.debug(error.description);
-                    }
-                }, this);
-            }
-        },
+            _execMf: function(mf, guid, cb) {
+                logger.debug(this.id + "._execMf");
+                if (mf && guid) {
+                    mx.ui.action(
+                        mf,
+                        {
+                            params: {
+                                applyto: "selection",
+                                guids: [guid]
+                            },
+                            callback: lang.hitch(this, function(objs) {
+                                if (cb && typeof cb === "function") {
+                                    cb(objs);
+                                }
+                            }),
+                            error: function(error) {
+                                console.debug(error.description);
+                            }
+                        },
+                        this
+                    );
+                }
+            },
 
-        // Rerender the interface.
-        _updateRendering: function (callback) {
-            logger.debug(this.id + "._updateRendering");
-          
+            // Rerender the interface.
+            _updateRendering: function(callback) {
+                logger.debug(this.id + "._updateRendering");
 
-            if (this._contextObj !== null) {
-                dojoStyle.set(this.domNode, "display", "block");
+                if (this._contextObj !== null) {
+                    dojoStyle.set(this.domNode, "display", "block");
+                }
+                // Important to clear all validations!
+                this._clearValidations();
 
-            }
-            // Important to clear all validations!
-            this._clearValidations();
+                // The callback, coming from update, needs to be executed, to let the page know it finished rendering
+                this._executeCallback(callback, "_updateRendering");
+            },
 
-            // The callback, coming from update, needs to be executed, to let the page know it finished rendering
-            this._executeCallback(callback, "_updateRendering");
-        },
+            // Handle validations.
+            _handleValidation: function(validations) {
+                logger.debug(this.id + "._handleValidation");
+                this._clearValidations();
 
-        // Handle validations.
-        _handleValidation: function (validations) {
-            logger.debug(this.id + "._handleValidation");
-            this._clearValidations();
+                var validation = validations[0],
+                    message = validation.getReasonByAttribute(
+                        this.backgroundColor
+                    );
 
-            var validation = validations[0],
-                message = validation.getReasonByAttribute(this.backgroundColor);
+                if (this._readOnly) {
+                    validation.removeAttribute(this.backgroundColor);
+                } else if (message) {
+                    this._addValidation(message);
+                    validation.removeAttribute(this.backgroundColor);
+                }
+            },
 
-            if (this._readOnly) {
-                validation.removeAttribute(this.backgroundColor);
-            } else if (message) {
-                this._addValidation(message);
-                validation.removeAttribute(this.backgroundColor);
-            }
-        },
+            // Clear validations.
+            _clearValidations: function() {
+                logger.debug(this.id + "._clearValidations");
+                dojoConstruct.destroy(this._alertDiv);
+                this._alertDiv = null;
+            },
 
-        // Clear validations.
-        _clearValidations: function () {
-            logger.debug(this.id + "._clearValidations");
-            dojoConstruct.destroy(this._alertDiv);
-            this._alertDiv = null;
-        },
-
-        // Show an error message.
-        _showError: function (message) {
-            logger.debug(this.id + "._showError");
-            if (this._alertDiv !== null) {
-                dojoHtml.set(this._alertDiv, message);
-                return true;
-            }
-            this._alertDiv = dojoConstruct.create("div", {
-                "class": "alert alert-danger",
-                "innerHTML": message
-            });
-            dojoConstruct.place(this._alertDiv, this.domNode);
-        },
-
-        // Add a validation.
-        _addValidation: function (message) {
-            logger.debug(this.id + "._addValidation");
-            this._showError(message);
-        },
-
-        // Reset subscriptions.
-        _resetSubscriptions: function () {
-            logger.debug(this.id + "._resetSubscriptions");
-            // Release handles on previous object, if any.
-            this.unsubscribeAll();
-
-            // When a mendix object exists create subscribtions.
-            if (this._contextObj) {
-                this.subscribe({
-                    guid: this._contextObj.getGuid(),
-                    callback: lang.hitch(this, function (guid) {
-                        this._updateRendering();
-                    })
+            // Show an error message.
+            _showError: function(message) {
+                logger.debug(this.id + "._showError");
+                if (this._alertDiv !== null) {
+                    dojoHtml.set(this._alertDiv, message);
+                    return true;
+                }
+                this._alertDiv = dojoConstruct.create("div", {
+                    class: "alert alert-danger",
+                    innerHTML: message
                 });
+                dojoConstruct.place(this._alertDiv, this.domNode);
+            },
 
-                this.subscribe({
-                    guid: this._contextObj.getGuid(),
-                    attr: this.backgroundColor,
-                    callback: lang.hitch(this, function (guid, attr, attrValue) {
-                        this._updateRendering();
-                    })
-                });
+            // Add a validation.
+            _addValidation: function(message) {
+                logger.debug(this.id + "._addValidation");
+                this._showError(message);
+            },
 
-                this.subscribe({
-                    guid: this._contextObj.getGuid(),
-                    val: true,
-                    callback: lang.hitch(this, this._handleValidation)
-                });
-            }
-        },
+            // Reset subscriptions.
+            _resetSubscriptions: function() {
+                logger.debug(this.id + "._resetSubscriptions");
+                // Release handles on previous object, if any.
+                this.unsubscribeAll();
 
-        _executeCallback: function (cb, from) {
-            logger.debug(this.id + "._executeCallback" + (from ? " from " + from : ""));
-            if (cb && typeof cb === "function") {
-                cb();
+                // When a mendix object exists create subscribtions.
+                if (this._contextObj) {
+                    this.subscribe({
+                        guid: this._contextObj.getGuid(),
+                        callback: lang.hitch(this, function(guid) {
+                            this._updateRendering();
+                        })
+                    });
+
+                    this.subscribe({
+                        guid: this._contextObj.getGuid(),
+                        attr: this.backgroundColor,
+                        callback: lang.hitch(this, function(
+                            guid,
+                            attr,
+                            attrValue
+                        ) {
+                            this._updateRendering();
+                        })
+                    });
+
+                    this.subscribe({
+                        guid: this._contextObj.getGuid(),
+                        val: true,
+                        callback: lang.hitch(this, this._handleValidation)
+                    });
+                }
+            },
+
+            _executeCallback: function(cb, from) {
+                logger.debug(
+                    this.id +
+                        "._executeCallback" +
+                        (from ? " from " + from : "")
+                );
+                if (cb && typeof cb === "function") {
+                    cb();
+                }
             }
         }
-    });
+    );
 });
 
 require(["agGridWidget/widget/agGridWidget"]);
